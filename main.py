987c6653011a0d2d557750441c6378de291d50fb
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2024-06-10 13:28:38 +0600

    day 26 initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# numbers = [1, 2, 3]
+# new_list = []
+# for n in numbers:
+#     add_1 = n + 1
+#     new_list.append(add_1)
+#
+# new_list = [n + 1 for n in numbers]
+# print(new_list)
+# name = "Angela"
+# new_list = [letter for letter in name ]
+# print(new_list)
+# # python sequences
+# # list range string tuple
+# new_list = [i*2 for i in range(1,5)]
+# print(new_list)
+
+# conditional list comprehension
+# new_list = [new_item for item in list if test]
+# new_list = [new_item for item in list if test]
+# new_list = [new_item for item in list if test]
+# new_list = [new_item for item in list if test]
+# new_list = [new_item for item in list if test]
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+# squared_numbers = [new_item for item in list]
+squared_numbers = [n**2 for n in numbers]
+print(squared_numbers)
+
